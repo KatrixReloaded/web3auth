c76d15a9e58769f2abd5ed0c1d20ab08914677c8
KatrixReloaded <kartikeyasureka@gmail.com>
2023-09-23 16:09:13 +0530

    signin page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import Head from "next/head";
+import axios from "axios";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-    async function getName() {}
+    async function getName() {
+        const response = await axios.get("/api/hello");
+        console.log(response);
+    }
     return (
         <main
             className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
